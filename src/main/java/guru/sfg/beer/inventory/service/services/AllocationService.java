5fe903b751c2acfbb532e5da051103140779e3b0
Paulo Roberto <paulorobertosp@gmail.com>
2021-04-20 15:34:31 -0300

    Creating allocation service

---
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationService.java
@@ -0,0 +1,8 @@
+package guru.sfg.beer.inventory.service.services;
+
+import br.com.prcompany.beerevents.model.BeerOrderDTO;
+
+public interface AllocationService {
+
+    boolean allocateOrder(BeerOrderDTO beerOrderDTO);
+}
